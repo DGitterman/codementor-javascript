78695e71aa52abfa3d66859d8920c4ba02bc6b27
Debra Gitterman <debra.gitterman@gmail.com>
2017-03-13 17:28:30 -0400

    First commit

---
+++ appendDate.js
@@ -0,0 +1,10 @@
+import moment from 'moment'
+
+let date = moment()
+
+let appendDate = () => { // function appendDate(){}
+  document.write(date)
+}
+
+
+export default appendDate
---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("File loaded.")
+let date = date
+import moment from 'moment'
+import appendDate from './appendDate'
+
+appendDate(date)
